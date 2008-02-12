d9a4abc281248676e5e4684a46973d8595ce16e8
collin.schroeder <collin.schroeder@46a719cb-f844-0410-885e-4986eec0b743>
2008-02-12 11:30:30 +0000

    W00t, I fixed the error.

--- trunk/GA Traveling Sales Person/GA Traveling Sales Person/OrderCX.cs
+++ trunk/GA Traveling Sales Person/GA Traveling Sales Person/OrderCX.cs
@@ -14,15 +14,15 @@ namespace GA_Traveling_Sales_Person
             myGraph = theGraph;
         }
         public Tour OrderCrossEm(Tour parentA, Tour parentB){
-            Int32 cityCount = parentA.Route.Length;
+            int cityCount = parentA.Route.Length;
             
             cityCount -= 1; //prevent out of bound exception
             int[] childA = parentA.Route;
             Random x = new Random();
-            Int32 cityA = x.Next(0, cityCount);
-            Int32 cityB = x.Next(0, cityCount);
-            Int32 laterCity = 0;
-            Int32 earlyCity = 0;
+            int cityA = x.Next(0, cityCount);
+            int cityB = x.Next(0, cityCount);
+            int laterCity = 0;
+            int earlyCity = 0;
             while (cityA == cityB)  cityB = x.Next(0, cityCount);
             if (cityA > cityB){
                 earlyCity = cityB;
@@ -33,15 +33,16 @@ namespace GA_Traveling_Sales_Person
                 earlyCity = cityA;
                 laterCity = cityB;
             }
-            Int32 interval = laterCity - earlyCity;
-            List<Int32> citysInInterval = new List<Int32>();
-            List<Int32> theChildRoute = new List<Int32>();
+            int interval = laterCity - earlyCity;
+            List<int> citysInInterval = new List<int>();
+            List<int> theChildRoute = new List<int>();
 
             for (int i = 0; i < interval; i++) citysInInterval.Add(parentA.Route[i + earlyCity]);
             
             theChildRoute.AddRange(citysInInterval);
-            Int32 trackFront =0;
-            foreach(Int32 cityInB in parentB.Route){
+            int trackFront = 0;
+            foreach (int cityInB in parentB.Route)
+            {
                    
                     if(theChildRoute.Contains(cityInB));
                     else{
