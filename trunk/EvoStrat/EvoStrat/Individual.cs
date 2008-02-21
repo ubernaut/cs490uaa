558252049ddc957c158730e442c2a335f08ffa28
jbuckland55 <jbuckland55@46a719cb-f844-0410-885e-4986eec0b743>
2008-02-21 05:32:55 +0000

    I got the main form UI layed out and a better plan for how to make the UI responsive while we're doing our runs.

---
+++ trunk/EvoStrat/EvoStrat/Individual.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvoStrat
+{
+    class Individual
+    {
+        List<double> x;
+
+        public List<double> X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        List<double> sigma;
+
+        public List<double> Sigma
+        {
+            get { return sigma; }
+            set { sigma = value; }
+        }
+
+
+
+
+
+
+
+
+
+        public override string ToString()
+        {
+
+            throw new Exception("Individual.ToString() not yet imlemented");
+
+            //string text = "";
+            //return text;
+        }
+
+    }
+}
